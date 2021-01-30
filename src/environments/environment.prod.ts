5b586e78bece1e15fea100223db2a49f5dca1f68
Raúl Morales <raulmdaw@gmail.com>
2021-01-30 18:36:00 +0100

    Se modifica acceso a producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBN0pEszEfiAf1LaaO3YwFolQJLoqXn7PY",
+    authDomain: "firecrud-ng.firebaseapp.com",
+    projectId: "firecrud-ng",
+    storageBucket: "firecrud-ng.appspot.com",
+    messagingSenderId: "116861373272",
+    appId: "1:116861373272:web:07b28058179321dea3f32d",
+    measurementId: "G-9GDRDZTQCP"
+  }
 };
